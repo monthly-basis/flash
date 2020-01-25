4910faf5b2df0e1cd8e62de80d10a41fb2570d49
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-01-25 09:15:06 +0000

    Improve unit test

--- test/Model/Service/FlashTest.php
+++ test/Model/Service/FlashTest.php
@@ -1,37 +1,32 @@
 <?php
 namespace LeoGalleguillos\FlashTest\Model\Service;
 
-use LeoGalleguillos\Flash\Model\Service\Flash as FlashService;
+use LeoGalleguillos\Flash\Model\Service as FlashService;
 use PHPUnit\Framework\TestCase;
 
 class FlashTest extends TestCase
 {
     protected function setUp()
     {
-        $this->flashService = new FlashService();
-    }
-
-    public function testInstance()
-    {
-        $this->assertInstanceOf(FlashService::class, $this->flashService);
+        $this->flashService = new FlashService\Flash();
     }
 
     public function testDestruct()
     {
-        $flashService1 = new FlashService();
+        $flashService1 = new FlashService\Flash();
         $flashService1->set('message', 'a');
         $this->assertSame('a', $flashService1->get('message'));
         unset($flashService1);
 
-        $flashService2 = new FlashService();
+        $flashService2 = new FlashService\Flash();
         $this->assertSame('a', $flashService2->get('message'));
         unset($flashService2);
 
-        $flashService3 = new FlashService();
+        $flashService3 = new FlashService\Flash();
         $this->assertEmpty($flashService3->get('message'));
         unset($flashService3);
 
-        $flashService4 = new FlashService();
+        $flashService4 = new FlashService\Flash();
         $this->assertEmpty($flashService4->get('message'));
     }
 
